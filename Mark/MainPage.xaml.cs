a3c0d8d7ebb73e3853225c7d995ca8ffd9ac2c0d
Henrik Lábadi <hlabadi@hotmail.com>
2020-06-13 20:05:43 +0200

    Add project files.

---
+++ Mark/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Mark.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Mark
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void WebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            if (args.Uri!=null)
+                args.Cancel = true;
+        }
+    }
+}
---
+++ Mark/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Mark.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+		private string _markdownText;
+
+		public string MarkdownText
+		{
+			get { return _markdownText; }
+			set { 
+				_markdownText = value;
+				OnPropertyChanged();
+				MarkdownChanged();
+			}
+		}
+
+		private string _htmlContent;
+
+		public string HtmlContent
+		{
+			get { return _htmlContent; }
+			set { if (string.IsNullOrWhiteSpace(value))
+					return;
+				_htmlContent = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public MainViewModel()
+		{
+			MarkdownText = string.Empty;
+			HtmlContent = string.Empty;
+		}
+
+		private void MarkdownChanged()
+		{
+			// render html
+			HtmlContent = Markdig.Markdown.ToHtml(_markdownText);
+		}
+	}
+}
